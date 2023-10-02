146a93b136bfbefd528ab9846e5987f62c540bad
Shahan Ameen <Shahanameen@gmail.com>
2023-10-02 17:07:30 -0500

    vars

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var time = document.getElementById("time")
+var startScreen = document.getElementById("start-screen")
+var startBtn = document.getElementById("startBtn")
+var questions = document.getElementById("questions")
+var questionTitle = document.getElementById("question-title")
+var choices = document.getElementById("choices")
+var endScreen = document.getElementById("end-screen")
+var finalScore = document.getElementById("final-score")
+var initials = document.getElementById("initials")
+var submit = document.getElementById("submit")
+
+
+
+
+
+
+
+
